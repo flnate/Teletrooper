2c2459ccd74af9533a47e2a144dba8c14a6fd6d5
Marcotte173 <47604041+Marcotte173@users.noreply.github.com>
2020-04-02 20:49:48 -0600

    Add files via upload
    Reshuffled, Organised files.
    Refactored code.
    Added health and Basic Health UI. Click on Player to damage him

---
+++ Assets/Scripts/CharacterInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterInfo : MonoBehaviour
+{
+    [SerializeField]
+    internal int health;
+    [SerializeField]
+    internal int jumpExtra;
+    [SerializeField]
+    internal float moveSpeed;
+    [SerializeField]
+    internal float jumpForce;
+}
