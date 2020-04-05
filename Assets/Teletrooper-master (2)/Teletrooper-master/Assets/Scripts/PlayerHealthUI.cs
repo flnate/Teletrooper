72510399a289b5958a00cb71d083c89475cc7819
spydino290 <62296195+spydino290@users.noreply.github.com>
2020-04-04 21:41:18 -0400

    Add files via upload

---
+++ Assets/Teletrooper-master (2)/Teletrooper-master/Assets/Scripts/PlayerHealthUI.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthUI : MonoBehaviour
+{
+    Text text;
+    Image bar;
+    public Health playerHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponentInChildren<Text>();
+        bar = GetComponentInChildren<Image>();
+    }
+
+    void Update()
+    {        
+        float fill = playerHealth._hp / playerHealth._maxHp;
+        string colour = (fill > .6f) ? "lime" : (fill > .3f) ? "yellow" : "red";
+        text.text = $"<color={colour}>{playerHealth._hp}/{playerHealth._maxHp}</color>";
+        bar.fillAmount = fill;
+    }
+}
